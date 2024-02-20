914f070ae6d55171b1d9b5b6d4efa80f333d3ae9
Leiladany <leiladany16@gmail.com>
2024-02-20 22:47:08 +0000

    finish explanation container

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Navbar from "../../components/Navbar/Navbar";
 import "./HomePage.css";
 
@@ -9,53 +10,55 @@ function HomePage() {
         <h2>O que é ?</h2>
         <h3>Uma loja de venda de roupa em 2º mão !</h3>
       </div>
-      <div className="explain-container">
-        <div className="step1">
-          <div className="icon-container">
-            <img src="/icon-1.png" alt="Description of the image" />
+      <div className="center-container">
+        <div className="explain-container">
+          <div className="step1">
+            <div className="icon-container">
+              <img src="/icon-1.png" alt="Description of the image" />
+            </div>
+            <div className="disc-container">
+              <h2>Criar o teu perfil</h2>
+              <h3>
+                Cria o teu perfil para guardares as tuas peças de roupa
+                favoritas e poderes contactar o vendedor.
+              </h3>
+            </div>
           </div>
-          <div className="disc-container">
-            <h2>Criar o teu perfil</h2>
-            <h3>
-              Cria o teu perfil para guardares as tuas peças de roupa favoritas
-              e poderes contactar o vendedor.
-            </h3>
+          <div className="step2">
+            <div className="icon-container">
+              <img src="/icon-2.png" alt="Description of the image" />
+            </div>
+            <div className="disc-container">
+              <h2>Procura a roupa que queres</h2>
+              <h3>
+                Explora nosso catálogo, caso não
+                queira comprometer-se já, guarde nos favoritos.
+              </h3>
+            </div>
           </div>
-        </div>
-        <div className="step2">
-          <div className="icon-container">
-            <img src="/icon-2.png" alt="Description of the image" />
+          <div className="step3">
+            <div className="icon-container">
+              <img src="/icon-3.png" alt="Description of the image" />
+            </div>
+            <div className="disc-container">
+              <h2>Veja os detalhes da peça que gostou.</h2>
+              <h3>
+                Clique nas peça que lhe chamou mais a atenção, caso seja do seu
+                agrado contacte o vendedor.
+              </h3>
+            </div>
           </div>
-          <div className="disc-container">
-            <h2>Criar o teu perfil</h2>
-            <h3>
-              Cria o teu perfil para guardares as tuas peças de roupa favoritas
-              e poderes contactar o vendedor.
-            </h3>
-          </div>
-        </div>
-        <div className="step3">
-          <div className="icon-container">
-            <img src="/icon-3.png" alt="Description of the image" />
-          </div>
-          <div className="disc-container">
-            <h2>Criar o teu perfil</h2>
-            <h3>
-              Cria o teu perfil para guardares as tuas peças de roupa favoritas
-              e poderes contactar o vendedor.
-            </h3>
-          </div>
-        </div>
-        <div className="step4">
-          <div className="icon-container">
-            <img src="/icon-4.png" alt="Description of the image" />
-          </div>
-          <div className="disc-container">
-            <h2>Criar o teu perfil</h2>
-            <h3>
-              Cria o teu perfil para guardares as tuas peças de roupa favoritas
-              e poderes contactar o vendedor.
-            </h3>
+          <div className="step4">
+            <div className="icon-container">
+              <img src="/icon-4.png" alt="Description of the image" />
+            </div>
+            <div className="disc-container">
+              <h2>Mande email ao vendedor</h2>
+              <h3>
+                Para finalizar clique em "Contactar Vendedor" e
+                escreva um email com o código da peça.
+              </h3>
+            </div>
           </div>
         </div>
       </div>
