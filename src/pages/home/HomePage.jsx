3eae1c3937ee86960f1fdcc3ebbdcb8472e1aafe
Leiladany <leiladany16@gmail.com>
2024-02-20 22:08:31 +0000

    create about and create explanations icon and description

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,64 @@
 import Navbar from "../../components/Navbar/Navbar";
+import "./HomePage.css";
 
 function HomePage() {
   return (
-    <div>
+    <div className="homepage-container">
       <Navbar />
-      <h2>Hello</h2>
+      <div className="about-container">
+        <h2>O que é ?</h2>
+        <h3>Uma loja de venda de roupa em 2º mão !</h3>
+      </div>
+      <div className="explain-container">
+        <div className="step1">
+          <div className="icon-container">
+            <img src="/icon-1.png" alt="Description of the image" />
+          </div>
+          <div className="disc-container">
+            <h2>Criar o teu perfil</h2>
+            <h3>
+              Cria o teu perfil para guardares as tuas peças de roupa favoritas
+              e poderes contactar o vendedor.
+            </h3>
+          </div>
+        </div>
+        <div className="step2">
+          <div className="icon-container">
+            <img src="/icon-2.png" alt="Description of the image" />
+          </div>
+          <div className="disc-container">
+            <h2>Criar o teu perfil</h2>
+            <h3>
+              Cria o teu perfil para guardares as tuas peças de roupa favoritas
+              e poderes contactar o vendedor.
+            </h3>
+          </div>
+        </div>
+        <div className="step3">
+          <div className="icon-container">
+            <img src="/icon-3.png" alt="Description of the image" />
+          </div>
+          <div className="disc-container">
+            <h2>Criar o teu perfil</h2>
+            <h3>
+              Cria o teu perfil para guardares as tuas peças de roupa favoritas
+              e poderes contactar o vendedor.
+            </h3>
+          </div>
+        </div>
+        <div className="step4">
+          <div className="icon-container">
+            <img src="/icon-4.png" alt="Description of the image" />
+          </div>
+          <div className="disc-container">
+            <h2>Criar o teu perfil</h2>
+            <h3>
+              Cria o teu perfil para guardares as tuas peças de roupa favoritas
+              e poderes contactar o vendedor.
+            </h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
