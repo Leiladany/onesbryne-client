44f51abfc5d87b7b624decf2490315c0336f95d9
Leiladany <leiladany16@gmail.com>
2024-03-05 18:57:01 +0000

    favorite icon and details on clothes-item

--- src/pages/clothesgrid/ClothesGrid.jsx
+++ src/pages/clothesgrid/ClothesGrid.jsx
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
-import './ClothesGrid.css';
+import "./ClothesGrid.css";
 
 const ClothesGrid = () => {
+  const [starClicked, setStarClicked] = useState({});
+
+  const handleStarClick = (index) => {
+    setStarClicked(prevState => ({
+      ...prevState,
+      [index]: !prevState[index]
+    }));
+  };
+
   return (
     <div className="page-container">
       <Navbar />
       <div className="clothes-grid-container">
         <div className="clothes-grid">
-          <div className="clothes-item">Item 1</div>
-          <div className="clothes-item">Item 2</div>
-          <div className="clothes-item">Item 3</div>
-          <div className="clothes-item">Item 4</div>
-          <div className="clothes-item">Item 5</div>
-          <div className="clothes-item">Item 6</div>
+          {[1, 2, 3, 4, 5, 6].map(index => (
+            <div className="clothes-item" key={index}>
+              <img
+                className="fav"
+                src={starClicked[index] ? "/star2.png" : "/star1.png"}
+                onClick={() => handleStarClick(index)}
+                alt="Star"
+              />
+              <div className="box">Item {index}</div>
+            </div>
+          ))}
         </div>
       </div>
       <Footer />
