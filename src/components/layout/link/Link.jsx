262dc135f74d308f795cd2ff9ae69e69c0e375e3
thetiagogil <talmeidagil@hotmail.com>
2024-04-24 20:13:56 +0100

    change Link component

--- src/components/layout/link/Link.jsx
+++ src/components/layout/link/Link.jsx
@@ -1,7 +1,7 @@
 import "./Link.css";
 import { Link } from "react-router-dom";
 
-const LinkComponent = ({ to, children, className, size }) => {
+const LinkComponent = ({ to, children, className, size, onClick }) => {
 
     const handleLinkClass = () => {
         let classes = [className ? `ui-control-link-${className}` : "", size ? `ui-control-link-size-${size}` : ""].filter(Boolean).join(" ");
@@ -9,7 +9,7 @@ const LinkComponent = ({ to, children, className, size }) => {
     };
 
     return (
-        <Link to={to} className={handleLinkClass()}>
+        <Link to={to} onClick={onClick} className={handleLinkClass()}>
             {children}
         </Link>
     );
