af5590a449f0f9c246aa055054f66f2f6b8f01a6
thetiagogil <talmeidagil@hotmail.com>
2024-05-05 22:39:55 +0100

    change navbar

--- src/components/navigation/navbar/components/Section3.jsx
+++ src/components/navigation/navbar/components/Section3.jsx
@@ -1,11 +1,12 @@
 'use client';
-import { Box, MenuButton, Menu, Stack, Dropdown, MenuItem } from '@mui/joy';
+import { Box, MenuButton, Menu, Stack, Dropdown, MenuItem, Typography } from '@mui/joy';
 import { useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import { AuthContext } from '../../../../contexts/AuthContext';
 import LinkComponent from '../../../layout/link/Link';
 
 // Icons
+import { IoIosLogOut } from 'react-icons/io';
 import { IoIosStar, IoIosStarOutline } from 'react-icons/io';
 import { GoPerson, GoPersonFill } from 'react-icons/go';
 import { PiDress, PiDressFill } from 'react-icons/pi';
@@ -35,6 +36,7 @@ const Section3 = () => {
       {!isAuthenticated ? (
         /* IS NOT AUTHENTICATED */
         <>
+          {/* DESKTOP */}
           <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 2 }}>
             <LinkComponent
               to="/signup"
@@ -44,6 +46,7 @@ const Section3 = () => {
             <LinkComponent to="/login" children="Entrar" className="line" />
           </Box>
 
+          {/* MOBILE */}
           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
             <Dropdown>
               <MenuButton variant="plain" sx={{ p: 0 }}>
@@ -81,54 +84,140 @@ const Section3 = () => {
       ) : (
         /* IS AUTHENTICATED */
         <>
-          {isAdmin && (
-            <LinkComponent to="/admin">
+          {/* DESKTOP */}
+          <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 2 }}>
+            {isAdmin && (
+              <LinkComponent to="/admin">
+                <Stack>
+                  {location.pathname === '/admin' ? (
+                    <MdAdminPanelSettings color="white" size={20} />
+                  ) : (
+                    <MdOutlineAdminPanelSettings color="white" size={20} />
+                  )}
+                </Stack>
+              </LinkComponent>
+            )}
+
+            <LinkComponent to="/clothes">
               <Stack>
-                {location.pathname === '/admin' ? (
-                  <MdAdminPanelSettings color="white" size={20} />
+                {location.pathname === '/clothes' ? (
+                  <PiDressFill color="white" size={20} />
                 ) : (
-                  <MdOutlineAdminPanelSettings color="white" size={20} />
+                  <PiDress color="white" size={20} />
                 )}
               </Stack>
             </LinkComponent>
-          )}
 
-          <LinkComponent to="/clothes">
-            <Stack>
-              {location.pathname === '/clothes' ? (
-                <PiDressFill color="white" size={20} />
-              ) : (
-                <PiDress color="white" size={20} />
-              )}
-            </Stack>
-          </LinkComponent>
+            <LinkComponent to="/favourites">
+              <Stack onClick={handleStarClick}>
+                {location.pathname === '/favourites' ? (
+                  <IoIosStar color="white" size={20} />
+                ) : (
+                  <IoIosStarOutline color="white" size={20} />
+                )}
+              </Stack>
+            </LinkComponent>
 
-          <LinkComponent to="/favourites">
-            <Stack onClick={handleStarClick}>
-              {location.pathname === '/favourites' ? (
-                <IoIosStar color="white" size={20} />
-              ) : (
-                <IoIosStarOutline color="white" size={20} />
-              )}
-            </Stack>
-          </LinkComponent>
+            <LinkComponent to="/profile">
+              <Stack>
+                {location.pathname === '/profile' ? (
+                  <GoPersonFill color="white" size={20} />
+                ) : (
+                  <GoPerson color="white" size={20} />
+                )}
+              </Stack>
+            </LinkComponent>
 
-          <LinkComponent to="/profile">
-            <Stack>
-              {location.pathname === '/profile' ? (
-                <GoPersonFill color="white" size={20} />
-              ) : (
-                <GoPerson color="white" size={20} />
-              )}
-            </Stack>
-          </LinkComponent>
+            <LinkComponent to="/" onClick={handleLogout}>
+              <Stack>
+                <IoIosLogOut color="white" size={20} />
+              </Stack>
+            </LinkComponent>
+          </Box>
 
-          <LinkComponent
-            to="/"
-            onClick={handleLogout}
-            children="Sair"
-            className="line"
-          />
+          {/* MOBILE */}
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <Dropdown>
+              <MenuButton variant="plain" sx={{ p: 0 }}>
+                <GoPerson color="white" size={25} />
+              </MenuButton>
+
+              <Menu
+                placement="bottom-end"
+                sx={{
+                  backgroundColor: 'neutral.800',
+                  p: 1,
+                  borderColor: 'neutral.700',
+                }}
+              >
+                <MenuItem>
+                  {isAdmin && (
+                    <LinkComponent to="/admin">
+                      <Stack sx={{flexDirection: "row", gap: 1}}>
+                        {location.pathname === '/admin' ? (
+                          <MdAdminPanelSettings color="white" size={20} />
+                        ) : (
+                          <MdOutlineAdminPanelSettings
+                            color="white"
+                            size={20}
+                          />
+                        )}
+                        <Typography>Admin</Typography>
+                      </Stack>
+                    </LinkComponent>
+                  )}
+                </MenuItem>
+
+                <MenuItem>
+                  <LinkComponent to="/clothes">
+                    <Stack sx={{flexDirection: "row", gap: 1}}>
+                      {location.pathname === '/clothes' ? (
+                        <PiDressFill color="white" size={20} />
+                      ) : (
+                        <PiDress color="white" size={20} />
+                      )}
+                      <Typography>Roupas</Typography>
+                    </Stack>
+                  </LinkComponent>
+                </MenuItem>
+
+                <MenuItem>
+                  <LinkComponent to="/favourites">
+                    <Stack sx={{flexDirection: "row", gap: 1}} onClick={handleStarClick}>
+                      {location.pathname === '/favourites' ? (
+                        <IoIosStar color="white" size={20} />
+                      ) : (
+                        <IoIosStarOutline color="white" size={20} />
+                      )}
+                      <Typography>Favoritos</Typography>
+                    </Stack>
+                  </LinkComponent>
+                </MenuItem>
+
+                <MenuItem>
+                  <LinkComponent to="/profile">
+                    <Stack sx={{flexDirection: "row", gap: 1}}>
+                      {location.pathname === '/profile' ? (
+                        <GoPersonFill color="white" size={20} />
+                      ) : (
+                        <GoPerson color="white" size={20} />
+                      )}
+                      <Typography>Perfil</Typography>
+                    </Stack>
+                  </LinkComponent>
+                </MenuItem>
+
+                <MenuItem>
+                  <LinkComponent to="/" onClick={handleLogout}>
+                    <Stack sx={{flexDirection: "row", gap: 1}}>
+                      <IoIosLogOut color="white" size={20} />
+                      <Typography>Sair</Typography>
+                    </Stack>
+                  </LinkComponent>
+                </MenuItem>
+              </Menu>
+            </Dropdown>
+          </Box>
         </>
       )}
     </Stack>
