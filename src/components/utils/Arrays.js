b9839a9f2fac90f604a00f335b85a8fb9aea0a40
thetiagogil <talmeidagil@hotmail.com>
2024-05-27 17:26:17 +0100

    update types array images

--- src/components/utils/Arrays.js
+++ src/components/utils/Arrays.js
@@ -1,38 +1,38 @@
 export const types = [
   {
-    type: "Tops",
-    img: "https://www.serebii.net/pokemongo/pokemon/001.png",
+    type: 'Tops',
+    img: 'https://i5.walmartimages.com/asr/023d976c-59ee-473a-a1db-937a117009f5.85e5d5151cd71f5d80ad8e0324cdde18.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF',
   },
   {
-    type: "T-Shirts",
-    img: "https://www.serebii.net/pokemongo/pokemon/004.png",
+    type: 'T-Shirts',
+    img: 'https://columbia.scene7.com/is/image/ColumbiaSportswear2/2033481_349_f_om?$x1_grid$&v=1714468679',
   },
   {
-    type: "Vestidos",
-    img: "https://www.serebii.net/pokemongo/pokemon/007.png",
+    type: 'Vestidos',
+    img: 'https://img.fruugo.com/product/6/92/608702926_max.jpg',
   },
   {
-    type: "Casacos",
-    img: "https://www.serebii.net/pokemongo/pokemon/010.png",
+    type: 'Casacos',
+    img: 'https://www.puntroma.com/medias/420153126FRON00.JPG?context=bWFzdGVyfGltYWdlc3wzMTk0MzJ8aW1hZ2UvanBlZ3xoNTUvaGVjLzExNDY1NzMxNDA3OTAyLzQyMDE1MzEyNkZST04wMC5KUEd8ZWUzM2M0NDIzODVlMTNjYjIyNmM2ZjNmNTY3MzZmZjkxNjZjMDIyYWY4MWRiOGYwZjg5Y2IxZDE1YmY1OTc4Nw',
   },
   {
-    type: "Pijamas",
-    img: "https://www.serebii.net/pokemongo/pokemon/013.png",
+    type: 'Pijamas',
+    img: 'https://img.fruugo.com/product/3/54/573434543_max.jpg',
   },
   {
-    type: "Desporto",
-    img: "https://www.serebii.net/pokemongo/pokemon/016.png",
+    type: 'Desporto',
+    img: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/1c6102d8-21e4-440a-a8c3-37597aa92209/sportswear-classic-high-waisted-7-8-leggings-fL87hV.png',
   },
   {
-    type: "Macacões",
-    img: "https://www.serebii.net/pokemongo/pokemon/019.png",
+    type: 'Macacões',
+    img: 'https://montetevardemenezes.pt/352026_wp-upload/1-Macac%C3%B5es-para-as-mulheres-bodysuit-sem-mangas-al%C3%A7as_picture.jpg',
   },
   {
-    type: "Bikinis",
-    img: "https://www.serebii.net/pokemongo/pokemon/025.png",
+    type: 'Bikinis',
+    img: 'https://i5.walmartimages.com/asr/270e791f-79d3-4dd6-b35f-f0d1e4771f2c.ba0acf51b5dd08a4f9ab30a3366f2000.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF',
   },
 ];
 
-export const sizes = ["XXS", "XS", "S", "M", "L", "XL", "XXL"];
+export const sizes = ['XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL'];
 
-export const statuses = ["Disponível", "Arquivado", "Vendido"]
+export const statuses = ['Disponível', 'Arquivado', 'Vendido'];
